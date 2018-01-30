b272d0df1a61df49bd99a3773e094c2bba5ee896
Yizhen <Yizhen@umich.edu>
2018-01-30 16:11:57 -0500

    pick an answer randomly

--- magic_eight.py
+++ magic_eight.py
@@ -1,5 +1,33 @@
 ####The Magic Ball Game
+import random
+
 def get_user_input():
     return input("What is your question?")
 
 answer = get_user_input()
+
+def get_answer():
+    Possible_Answer = [ "It is certain",
+                    "It is decidedly so",
+                    "Without a doubt",
+                    "Yes definitely",
+                    "You may rely on it",
+                    "As I see it, yes",
+                    "Most likely",
+                    "Outlook good",
+                    "Yes",
+                    "Signs point to yes",
+                    "Reply hazy try again",
+                    "Ask again later",
+                    "Better not tell you now",
+                    "Cannot predict now",
+                    "Concentrate and ask again",
+                    "Don't count on it",
+                    "My reply is no",
+                    "My sources say no",
+                    "Outlook not so good",
+                    "Very doubtful"]
+
+    print(random.choice(Possible_Answer))
+
+possible = get_answer()
