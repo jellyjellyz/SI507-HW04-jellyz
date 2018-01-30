64242df73ce55a8a05915d9ac25f31325be41d92
Jiayu Zhang <jellyz@umich.edu>
2018-01-30 16:10:11 -0500

    jelly add the magic_8 method

--- magic_eight.py
+++ magic_eight.py
@@ -1,5 +1,23 @@
 ####The Magic Ball Game
 def get_user_input():
-    return input("What is your question?")
+	return input("What is your question?")
 
-answer = get_user_input()
+def get_answer():
+	return 0
+
+def magic_8():
+	check = True
+	while check:
+		answer = get_user_input()
+		answer.strip()
+		if answer[-1]=='?':
+			print(get_answer())
+			if input("Do you want to quit? type in quit.").lower() == "quit":
+				check = False
+
+			else:
+				check = True
+
+	
+
+magic_8()
